854f741f52610f7c2b3a78cddb976433f5b7cd09
SOS69069 <tarumora@gmail.com>
2026-08-02 15:46:44 +0300

    Refactor contract.js to streamline wallet connection

--- contract.js
+++ contract.js
@@ -1,84 +1,53 @@
 const SOS_CONTRACT =
 "0x61af906f53Eb927790055AC8eA99916a01873c15";
 
-
 const SOS_ABI = [
+    [{"inputs":[],"name":"NoETHAccepted","type":"error"},{"inputs":[],"name":"PushToZeroAddress","type":"error"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"prevTotalSupply","type":"uint256"}],"name":"PhaseOverflow","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"prevPushOf","type":"uint256"}],"name":"PushOverflow","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Pushed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":false,"internalType":"uint256","name":"prevBalance","type":"uint256"}],"name":"TrustOfOverflow","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"displayBalanceOf","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myDisplayBalance","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myPushCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myPushOverflowCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myTrustOverflowCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"pushCountOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pushForMe","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"pushOverflowCountOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"pushTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"totalPhaseOverflowCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"trustOverflowCountOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"uint256Max","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"verifyTotalSupply","outputs":[{"internalType":"uint256","name":"storedTotal","type":"uint256"}],"stateMutability":"view","type":"function"},{"stateMutability":"payable","type":"receive"}]
+];
 
-{
-    "inputs":[
-        {
-            "internalType":"address",
-            "name":"",
-            "type":"address"
-        }
-    ],
-    "name":"trustOf",
-    "outputs":[
-        {
-            "internalType":"uint256",
-            "name":"",
-            "type":"uint256"
-        }
-    ],
-    "stateMutability":"view",
-    "type":"function"
-},
+let provider = null;
+let signer = null;
+let contract = null;
 
+async function connectWallet() {
 
-{
-    "inputs":[
-        {
-            "internalType":"address",
-            "name":"",
-            "type":"address"
-        }
-    ],
-    "name":"pushOf",
-    "outputs":[
-        {
-            "internalType":"uint256",
-            "name":"",
-            "type":"uint256"
-        }
-    ],
-    "stateMutability":"view",
-    "type":"function"
-},
+    if (!window.ethereum) {
+        throw new Error("MetaMask Mobile not found.");
+    }
 
+    provider = new ethers.BrowserProvider(window.ethereum);
 
-{
-    "inputs":[
-        {
-            "internalType":"address",
-            "name":"",
-            "type":"address"
-        }
-    ],
-    "name":"balanceOf",
-    "outputs":[
-        {
-            "internalType":"uint256",
-            "name":"",
-            "type":"uint256"
-        }
-    ],
-    "stateMutability":"view",
-    "type":"function"
-},
+    await provider.send("eth_requestAccounts", []);
 
+    signer = await provider.getSigner();
 
-{
-    "inputs":[
-        {
-            "internalType":"address",
-            "name":"to",
-            "type":"address"
-        }
-    ],
-    "name":"pushTo",
-    "outputs":[],
-    "stateMutability":"nonpayable",
-    "type":"function"
+    contract = new ethers.Contract(
+        SOS_CONTRACT,
+        SOS_ABI,
+        signer
+    );
+
+    return await signer.getAddress();
 }
 
-];
+async function getTrust(address) {
+    return await contract.balanceOf(address);
+}
+
+async function getPushCount(address) {
+    return await contract.pushCountOf(address);
+}
+
+async function getTotalSupply() {
+    return await contract.totalSupply();
+}
+
+async function pushTo(receiver) {
+    const tx = await contract.pushTo(receiver);
+    return await tx.wait();
+}
+
+async function pushForMe() {
+    const tx = await contract.pushForMe();
+    return await tx.wait();
+}
