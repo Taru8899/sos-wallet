d378f4594ce662215690de71fc58f9dcb03ab88f
Taru8899 <tarumora@gmail.com>
2026-08-02 15:07:43 +0300

    Add SOS contract address and ABI definition
    Define SOS_CONTRACT address and SOS_ABI for contract interactions.

---
+++ contract.js
@@ -0,0 +1,69 @@
+const SOS_CONTRACT =
+"0x61af906f53Eb927790055AC8eA99916a01873c15";
+
+
+const SOS_ABI = [
+
+    {
+        "inputs":[
+            {
+                "internalType":"address",
+                "name":"",
+                "type":"address"
+            }
+        ],
+        "name":"trustOf",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    },
+
+
+    {
+        "inputs":[
+            {
+                "internalType":"address",
+                "name":"",
+                "type":"address"
+            }
+        ],
+        "name":"pushOf",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    },
+
+
+    {
+        "inputs":[
+            {
+                "internalType":"address",
+                "name":"",
+                "type":"address"
+            }
+        ],
+        "name":"balanceOf",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    }
+
+];
