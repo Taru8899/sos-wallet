a61e7505f841eb79c7e4f8a44238e337f0a0b226
SOS69069 <tarumora@gmail.com>
2026-08-02 16:09:17 +0300

    Refactor contract.js for improved readability

--- contract.js
+++ contract.js
@@ -1,52 +1,209 @@
 const SOS_CONTRACT =
 "0x61af906f53Eb927790055AC8eA99916a01873c15";
 
+
 const SOS_ABI = [
-{"inputs":[],"name":"NoETHAccepted","type":"error"},{"inputs":[],"name":"PushToZeroAddress","type":"error"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"prevTotalSupply","type":"uint256"}],"name":"PhaseOverflow","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"prevPushOf","type":"uint256"}],"name":"PushOverflow","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Pushed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":false,"internalType":"uint256","name":"prevBalance","type":"uint256"}],"name":"TrustOfOverflow","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"displayBalanceOf","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myDisplayBalance","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myPushCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myPushOverflowCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"myTrustOverflowCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"pushCountOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pushForMe","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"pushOverflowCountOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"pushTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"totalPhaseOverflowCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"trustOverflowCountOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"uint256Max","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"verifyTotalSupply","outputs":[{"internalType":"uint256","name":"storedTotal","type":"uint256"}],"stateMutability":"view","type":"function"},{"stateMutability":"payable","type":"receive"}];
+
+    {
+        "inputs":[
+            {
+                "internalType":"address",
+                "name":"account",
+                "type":"address"
+            }
+        ],
+        "name":"balanceOf",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    },
+
+    {
+        "inputs":[
+            {
+                "internalType":"address",
+                "name":"user",
+                "type":"address"
+            }
+        ],
+        "name":"pushCountOf",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    },
+
+    {
+        "inputs":[],
+        "name":"totalSupply",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    },
+
+    {
+        "inputs":[
+            {
+                "internalType":"address",
+                "name":"to",
+                "type":"address"
+            }
+        ],
+        "name":"pushTo",
+        "outputs":[],
+        "stateMutability":"nonpayable",
+        "type":"function"
+    },
+
+    {
+        "inputs":[],
+        "name":"pushForMe",
+        "outputs":[],
+        "stateMutability":"nonpayable",
+        "type":"function"
+    }
+
+];
+
 
 let provider = null;
 let signer = null;
 let contract = null;
 
-async function connectWallet() {
 
-    if (!window.ethereum) {
-        throw new Error("MetaMask Mobile not found.");
+
+async function connectWallet(){
+
+    if(!window.ethereum){
+
+        throw new Error(
+        "MetaMask not detected"
+        );
+
     }
 
-    provider = new ethers.BrowserProvider(window.ethereum);
 
-    await provider.send("eth_requestAccounts", []);
+    provider =
+    new ethers.BrowserProvider(
+        window.ethereum
+    );
 
-    signer = await provider.getSigner();
 
-    contract = new ethers.Contract(
+    await provider.send(
+        "eth_requestAccounts",
+        []
+    );
+
+
+    signer =
+    await provider.getSigner();
+
+
+    contract =
+    new ethers.Contract(
         SOS_CONTRACT,
         SOS_ABI,
         signer
     );
 
+
+    console.log(
+        "Contract connected:",
+        SOS_CONTRACT
+    );
+
+
     return await signer.getAddress();
+
 }
 
-async function getTrust(address) {
-    return await contract.balanceOf(address);
+
+
+async function getTrust(address){
+
+    const result =
+    await contract.balanceOf(address);
+
+    console.log(
+        "Trust:",
+        result.toString()
+    );
+
+    return result;
+
 }
 
-async function getPushCount(address) {
-    return await contract.pushCountOf(address);
+
+
+async function getPushCount(address){
+
+    const result =
+    await contract.pushCountOf(address);
+
+
+    console.log(
+        "Push:",
+        result.toString()
+    );
+
+
+    return result;
+
 }
 
-async function getTotalSupply() {
-    return await contract.totalSupply();
+
+
+async function getTotalSupply(){
+
+    const result =
+    await contract.totalSupply();
+
+
+    console.log(
+        "Supply:",
+        result.toString()
+    );
+
+
+    return result;
+
 }
 
-async function pushTo(receiver) {
-    const tx = await contract.pushTo(receiver);
+
+
+async function pushTo(receiver){
+
+    const tx =
+    await contract.pushTo(receiver);
+
     return await tx.wait();
+
 }
 
-async function pushForMe() {
-    const tx = await contract.pushForMe();
+
+
+async function pushForMe(){
+
+    const tx =
+    await contract.pushForMe();
+
     return await tx.wait();
+
 }
